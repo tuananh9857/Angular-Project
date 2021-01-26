6a3feba6b9fef13a9f489a0f65ddfa7dcaa1b6d2
Vu <tuananh9857@gmail.com>
2021-01-26 13:14:42 -0800

    add

--- project1/src/app/app.module.ts
+++ project1/src/app/app.module.ts
@@ -3,16 +3,32 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { InfoPageComponent } from './info-page/info-page.component';
+import { MainPageComponent } from './main-page/main-page.component';
+import { RouterModule, Routes } from '@angular/router';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 
+const routes: Routes =[
+  {path:'info-component', component:InfoPageComponent},
+  {path:'main-component', component:MainPageComponent},
+  {path:'**', component:PageNotFoundComponent}
+];
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    InfoPageComponent,
+    MainPageComponent,
+    PageNotFoundComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    RouterModule.forRoot(routes)
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
+  exports:[
+    RouterModule
+  ]
 })
 export class AppModule { }
